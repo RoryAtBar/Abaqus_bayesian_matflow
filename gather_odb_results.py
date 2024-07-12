d615b661ab6b5dfd88d32fa4d1b9fd7fab9a9428
Gerard Capes <gerard.capes@manchester.ac.uk>
2024-07-12 10:40:02 +0100

    Group gathered results by variable instead of iteration

--- gather_odb_results.py
+++ gather_odb_results.py
@@ -3,12 +3,23 @@ from pathlib import Path
 
 def gather_odb_results(combined_odb_results: list):
     """Gather results from each combination of parameters from a sequence"""
-#    results = []
-#    for result in odb_results:
-#      results.append(result)
-#      # combine list of dictionaries into list of dictionary data
-#      # eventually want a dict of lists
+    force_vals1 = []
+    force_vals2 = []
+    barrelling_profile = []
+    peeq_vals = []
+    nt11 = []
+    for result in combined_odb_results:
+      force_vals1.append(result["force_vals1"])
+      force_vals2.append(result["force_vals2"])
+      barrelling_profile.append(result["barrelling_profile"])
+      peeq_vals.append(result["peeq_vals"])
+      nt11.append(result["nt11"])
+    gathered_odb_results = {"force_vals1": force_vals1,
+                            "force_vals2": force_vals2,
+                            "barrelling_profile": barrelling_profile,
+                            "peeq_vals": peeq_vals,
+                            "nt11": nt11}
     with open("odb_results_table.json", "w") as f:
-        json.dump(combined_odb_results, f, indent=2)
-    summary_table = combined_odb_results 
-    return {"odb_results_table": summary_table}
+        json.dump(gathered_odb_results, f, indent=2)
+
+    return {"odb_results_table": gathered_odb_results}
