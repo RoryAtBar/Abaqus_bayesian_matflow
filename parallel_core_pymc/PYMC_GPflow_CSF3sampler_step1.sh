b1826ff762312c873e0e22e94821df7705c02656
Gerard Capes <gerard.capes@manchester.ac.uk>
2024-09-03 09:48:58 +0100

    Update paths for my CSF account

--- parallel_core_pymc/PYMC_GPflow_CSF3sampler_step1.sh
+++ parallel_core_pymc/PYMC_GPflow_CSF3sampler_step1.sh
@@ -3,12 +3,11 @@
 # SGE options (whose lines must begin with #$)
 
 #$ -cwd               # Run the job in the current directory 
-source activate PYMC_GPFlow
-
+source ../test_venv/bin/activate
 
 
 #!/bin/sh
-OUTPUTFILE="MCMC_test_single_out_autokernel"
+OUTPUTDIR=~/scratch/MCMC_test_single_out_autokernel
 MAINSCRIPT="PYMC_GPflow_CSF3sampler_step1.py"
 XNORMALISER="Quad_plast_800C001_X.pkl"
 YNORMALISER="Quad_plast_800C001_Y.pkl"
@@ -17,9 +16,9 @@ YDATA="Y_train.pkl"
 XVALIDATE="X_validate.pkl"
 YVALIDATE="Y_validate.pkl"
 MODEL_NAME="Quadplast_timesteps_5-15"
-MODELFOLDER="/mnt/iusers01/jf01/w10944rb/saved_models/Chapt3/Quad_plast/800C_001s-1/"
-SUBFOLDER="/mnt/iusers01/jf01/w10944rb/GP_surrogate_pymc/parallel_core_pymc/"
-VARDAT="Ti64_Flowcurves/"
+MODELFOLDER=/mnt/iusers01/support/mbexegc2/scratch/MCMC_test
+SUBFOLDER=/mnt/iusers01/support/mbexegc2/scratch/Abaqus_bayesian_matflow/parallel_core_pymc
+VARDAT=/mnt/iusers01/support/mbexegc2/scratch/Abaqus_bayesian_matflow/parallel_core_pymc/Ti64_Flowcurves
 KERNEL="Auto"
 BASIS_FUNCS=2
 PLASTIC_START=4
@@ -27,15 +26,15 @@ PLASTIC_END=15
 
 
 
-mkdir ~/scratch/"${OUTPUTFILE}"
+mkdir "${OUTPUTDIR}"
 cd "${MODELFOLDER}"
-cp "${MODELFOLDER}${XDATA}" ~/scratch/"${OUTPUTFILE}"
-cp "${MODELFOLDER}${YDATA}" ~/scratch/"${OUTPUTFILE}"
-cp "${MODELFOLDER}${XVALIDATE}" ~/scratch/"${OUTPUTFILE}"
-cp "${MODELFOLDER}${YVALIDATE}" ~/scratch/"${OUTPUTFILE}"
-cp "${MODELFOLDER}${XNORMALISER}" ~/scratch/"${OUTPUTFILE}"
-cp "${MODELFOLDER}${YNORMALISER}" ~/scratch/"${OUTPUTFILE}"
-cp -r "${VARDAT}" ~/scratch/"${OUTPUTFILE}"
-cp "${SUBFOLDER}${MAINSCRIPT}" ~/scratch/"${OUTPUTFILE}"
-cd ~/scratch/"${OUTPUTFILE}"
-python "${SUBFOLDER}${MAINSCRIPT}" "${XDATA}" "${YDATA}" "${XVALIDATE}" "${YVALIDATE}" "${XNORMALISER}" "${YNORMALISER}" "${BASIS_FUNCS}" "${KERNEL}" "${MODEL_NAME}"
+cp "${MODELFOLDER}/${XDATA}" "${OUTPUTDIR}"
+cp "${MODELFOLDER}/${YDATA}" "${OUTPUTDIR}"
+cp "${MODELFOLDER}/${XVALIDATE}" "${OUTPUTDIR}"
+cp "${MODELFOLDER}/${YVALIDATE}" "${OUTPUTDIR}"
+cp "${MODELFOLDER}/${XNORMALISER}" "${OUTPUTDIR}"
+cp "${MODELFOLDER}/${YNORMALISER}" "${OUTPUTDIR}"
+cp -r "${VARDAT}/" "${OUTPUTDIR}"
+cp "${SUBFOLDER}/${MAINSCRIPT}" "${OUTPUTDIR}"
+cd "${OUTPUTDIR}"
+python "${SUBFOLDER}/${MAINSCRIPT}" "${XDATA}" "${YDATA}" "${XVALIDATE}" "${YVALIDATE}" "${XNORMALISER}" "${YNORMALISER}" "${BASIS_FUNCS}" "${KERNEL}" "${MODEL_NAME}"
