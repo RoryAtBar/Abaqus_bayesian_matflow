feba013e1a4bf3abc830867a6d12f36fb8a6364c
Gerard Capes <gerard.capes@manchester.ac.uk>
2024-09-05 10:20:08 +0100

    Update paths for my CSF3 account

--- parallel_core_pymc/PYMC_GPflow_CSF3sampler_step2.sh
+++ parallel_core_pymc/PYMC_GPflow_CSF3sampler_step2.sh
@@ -3,20 +3,21 @@
 # SGE options (whose lines must begin with #$)
 
 #$ -cwd               # Run the job in the current directory 
-source activate PYMC_GPFlow
 #$ -l mem2000
 #$ -hold_jid PYMC_GPflow_CSF3sampler_step1.sh 
 #$ -t 1-25
 
+source ../test_venv/bin/activate
+
 
 #!/bin/sh
-OUTPUTFILE="MCMC_test_single_out_autokernel"
+OUTPUTDIR=~/scratch/MCMC_test_single_out_autokernel
 MAINSCRIPT="PYMC_GPflow_CSF3sampler_v2pkl.py"
 XNORMALISER="Quad_plast_800C001_X.pkl"
 YNORMALISER="Quad_plast_800C001_Y.pkl"
 
 MODEL="Quadplast_timesteps_5-15.pkl"
-SUBFOLDER="/mnt/iusers01/jf01/w10944rb/GP_surrogate_pymc/parallel_core_pymc/"
+SUBFOLDER=/mnt/iusers01/support/mbexegc2/scratch/Abaqus_bayesian_matflow/parallel_core_pymc
 VARDAT="Ti64_Flowcurves/"
 BASIS_FUNCS=2
 PLASTIC_START=4
@@ -26,5 +27,5 @@ SAMPLE=200000
 COEFFS="46973769.8 322884.150"
 
 
-cd ~/scratch/"${OUTPUTFILE}"
-python "${SUBFOLDER}${MAINSCRIPT}" "${SGE_TASK_ID}" "${MODEL}" "${XNORMALISER}" "${YNORMALISER}" "${VARDAT}" "${BASIS_FUNCS}" "${PLASTIC_START}" "${PLASTIC_END}" "${BURN_IN}" "${SAMPLE}" "${COEFFS}"
+cd ${OUTPUTDIR}
+python "${SUBFOLDER}/${MAINSCRIPT}" "${SGE_TASK_ID}" "${MODEL}" "${XNORMALISER}" "${YNORMALISER}" "${VARDAT}" "${BASIS_FUNCS}" "${PLASTIC_START}" "${PLASTIC_END}" "${BURN_IN}" "${SAMPLE}" "${COEFFS}"
