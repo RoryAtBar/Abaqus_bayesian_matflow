c7bcfbb42eaee88c66f6a9e9013393faa8d52f67
Gerard Capes <gerard.capes@manchester.ac.uk>
2024-09-10 15:57:11 +0100

    Update 3rd script with CSF file paths

--- parallel_core_pymc/PYMC_GPflow_CSF3sampler_step3.sh
+++ parallel_core_pymc/PYMC_GPflow_CSF3sampler_step3.sh
@@ -3,17 +3,17 @@
 # SGE options (whose lines must begin with #$)
 
 #$ -cwd               # Run the job in the current directory 
-source activate PYMC_GPFlow
+source ../test_venv/bin/activate
 #$ -hold_jid PYMC_GPflow_CSF3sampler_step2.sh 
 
 
 
 #!/bin/sh
-OUTPUTFILE="MCMC_test_single_out_autokernel"
+OUTPUTDIR=~/scratch/MCMC_test_single_out_autokernel
 MAINSCRIPT="PYMC_GPflow_CSF3sampler_step3.py"
-SUBFOLDER="/mnt/iusers01/jf01/w10944rb/GP_surrogate_pymc/parallel_core_pymc/"
+SUBFOLDER=/mnt/iusers01/support/mbexegc2/scratch/Abaqus_bayesian_matflow/parallel_core_pymc
 COMBINEDTRACEFILENAME="Quad_plast_25_chain"
 
 
-cd ~/scratch/"${OUTPUTFILE}"
-python "${SUBFOLDER}${MAINSCRIPT}" "${COMBINEDTRACEFILENAME}" 
+cd ${OUTPUTDIR}
+python ${SUBFOLDER}/${MAINSCRIPT} "${COMBINEDTRACEFILENAME}"
